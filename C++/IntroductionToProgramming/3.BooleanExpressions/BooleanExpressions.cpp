d6454e3ec0ae2de9c6e61a90db7e5b206b35b439
AneliyaPPetkova <aneliyap.petkova@gmail.com>
2016-10-16 22:39:22 +0300

    BooleanExpression - added solution to a problem of finding if a quadratic equation doesn't have roots.

--- C++/IntroductionToProgramming/3.BooleanExpressions/BooleanExpressions.cpp
+++ C++/IntroductionToProgramming/3.BooleanExpressions/BooleanExpressions.cpp
@@ -2,24 +2,57 @@
 using namespace std;
 
 bool pDividesTo4Or7(int p);
+bool theQuadraticEquationHasNoRoots(float a, float b, float c);
 
 int main()
 {
+	//a)Find if the integer number p divides to 4 and 7
 	int p;
 	cout << "a)Find if the integer number p divides to 4 and 7: " << endl;
 	cout << "Enter p = ";
 	cin >> p;
 	cout << "Result: " << boolalpha << pDividesTo4Or7(p) << endl << "\n";
 
+	//b)Find if a quadratic equation has no roots
+	float a, b, c;
+	cout << "b)The quadratic equation has no roots: " << endl;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	cout << "Result: " << boolalpha << theQuadraticEquationHasNoRoots(a, b, c) << endl << "\n";
+
 	return 0;
 }
 
-//Find if the integer number p divides to 4 and 7
+//a)Find if the integer number p divides to 4 and 7
 bool pDividesTo4Or7(int p)
 {
 	bool result;
 
 	result = ((p % 4) == 0 || (p % 7) == 0);
+
+	return result;
+}
+
+//b)Find if a quadratic equation has no roots
+bool theQuadraticEquationHasNoRoots(float a, float b, float c)
+{
+	bool result;
+	float D;
+
+	D = b*b - 4 * a*c;
+
+	if (D >= 0)
+	{
+		result = false;
+	}
+	else
+	{
+		result = true;
+	}
 
 	return result;
 }
