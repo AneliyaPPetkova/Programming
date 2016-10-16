e9d519a3cf38ad26b56281371e0a8b75bb334272
AneliyaPPetkova <aneliyap.petkova@gmail.com>
2016-10-16 12:59:15 +0300

    Added solution for getting the Square Area and Perimeter when the side of the square is known.

---
+++ C++/IntroductionToProgramming/SquareAreaPerimeter/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, P, S;
+
+	cout << "a = ";
+	cin >> a;
+	
+	P = 4 * a;
+	S = a * a;
+
+	cout << "A square with side a = " << a << " has Perimeter P = " << P << " and Area S = " << S << endl;
+
+	return 0;
+}
